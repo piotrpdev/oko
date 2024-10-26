ef3ad3cdc1537943aa334ba07a6a77c03a543e8a
Piotr Płaczek <piotrpdev@gmail.com>
2024-10-26 23:48:50 +0100

    refactor: OKO-46 remove test data in migrations

--- migrations/20231108213118_init.sql
+++ migrations/20231108213118_init.sql
@@ -53,26 +53,4 @@ CREATE INDEX IF NOT EXISTS idx_videos_camera ON videos (camera_id);
 CREATE INDEX IF NOT EXISTS idx_camera_settings_camera ON camera_settings (camera_id);
 
 INSERT INTO users (user_id, username, password_hash, created_at) VALUES
-    (1, 'admin', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw', '2024-10-21 17:01:23'),
-    (2, 'piotrpdev', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw', '2024-10-21 17:02:18'),
-    (3, 'joedaly', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw', '2024-10-21 17:12:32');
-
-INSERT INTO cameras (camera_id, name, ip_address, last_connected) VALUES
-    (1, 'Front Door', '192.168.0.169', '2024-10-20 17:56:18'),
-    (2, 'Kitchen', '192.168.0.172', '2024-10-20 17:57:22');
-
-INSERT INTO camera_permissions (camera_id, user_id, can_view, can_control) VALUES
-    (1, 1, true, true),
-    (2, 1, true, true),
-    (1, 2, true, true),
-    (2, 2, true, true),
-    (1, 3, true, false),
-    (2, 3, false, false);
-
-INSERT INTO videos (camera_id, file_path, start_time, end_time, file_size) VALUES
-    (1, '/home/piotrpdev/oko/scripts/1.mp4', '2024-10-21 02:58:32', '2024-10-21 03:01:12', 6762403),
-    (2, '/home/piotrpdev/oko/scripts/2.mp4', '2024-10-21 02:57:56', '2024-10-21 03:03:23', 6905856);
-
-INSERT INTO camera_settings (camera_id, flashlight_enabled, resolution, framerate, last_modified, modified_by) VALUES
-    (1, false, '800x600', 5, '2024-10-21 17:02:33', 1),
-    (2, false, '800x600', 5, '2024-10-21 17:02:25', 1);
+    (1, 'admin', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw', '2024-10-21 17:01:23');
--- src/web/protected.rs
+++ src/web/protected.rs
@@ -11,8 +11,6 @@ struct ProtectedTemplate<'a> {
     username: &'a str,
     user_json: &'a str,
     cameras_json: &'a str,
-    video_camera_name: &'a str,
-    videos_for_camera: &'a str
 }
 
 pub fn router() -> Router<()> {
@@ -20,7 +18,7 @@ pub fn router() -> Router<()> {
 }
 
 mod get {
-    use crate::db::{Camera, Video, User};
+    use crate::db::{Camera, User};
 
     use super::*;
 
@@ -32,22 +30,13 @@ mod get {
                     .await
                     .unwrap();
 
-                let camera_id = 1;
-
-                let videos =
-                    Video::list_for_camera(&auth_session.backend.db, camera_id)
-                    .await
-                    .unwrap();
-
                 let safe_user = User::to_redacted_clone(&user);
 
                 ProtectedTemplate {
                     messages: messages.into_iter().collect(),
                     username: &user.username,
                     user_json: &serde_json::to_string(&safe_user).unwrap(),
-                    cameras_json: &serde_json::to_string(&cameras).unwrap(),
-                    video_camera_name: &camera_id.to_string(),
-                    videos_for_camera: &serde_json::to_string(&videos).unwrap()
+                    cameras_json: &serde_json::to_string(&cameras).unwrap()
                 }
                 .into_response()
             }
