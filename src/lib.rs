7dd94c4ecee1ac8c559fcda9b4f5322c262820eb
Piotr Płaczek <piotrpdev@gmail.com>
2024-10-28 01:49:51 +0000

    refactor: OKO-46 use `lib.rs`
    Reference: https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html?highlight=lib.rs#separation-of-concerns-for-binary-projects

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+use crate::web::App;
+
+mod users;
+mod web;
+mod db;
+
+pub use {
+    db::Camera,
+    db::CameraPermission,
+    db::CameraPermissionView,
+    db::CameraSetting,
+    db::User,
+    db::Video,
+    db::VideoCameraView,
+};
+
+pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(
+            |_| "axum_login=debug,tower_sessions=debug,sqlx=warn,tower_http=debug".into(),
+        )))
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
+
+    App::new().await?.serve().await
+}
