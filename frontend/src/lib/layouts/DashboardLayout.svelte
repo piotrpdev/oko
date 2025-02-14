8a81c81799f47787c17090f258c1a45fb07273b7
Piotr Płaczek <piotrpdev@gmail.com>
2025-02-14 17:03:00 +0000

    fix: missing z-index on sticky header

--- frontend/src/lib/layouts/DashboardLayout.svelte
+++ frontend/src/lib/layouts/DashboardLayout.svelte
@@ -32,13 +32,12 @@
 
 <div class="flex min-h-screen w-full flex-col">
   <header
-    class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6"
+    class="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6"
   >
     <nav
       class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
     >
       <span class="flex items-center gap-2 text-lg font-semibold md:text-base">
-        <!-- TODO: Replace with logo -->
         <Eye class="h-6 w-6" />
         <span class="sr-only">Oko</span>
       </span>
