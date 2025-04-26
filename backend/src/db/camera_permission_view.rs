3cf75828d884335b2cb37ed457a2888a1f8ccafc
Piotr Płaczek <piotrpdev@gmail.com>
2025-04-26 20:09:13 +0100

    refactor: OKO-107 add ip to `CameraPermissionView`

--- backend/src/db/camera_permission_view.rs
+++ backend/src/db/camera_permission_view.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 pub struct CameraPermissionView {
     pub camera_id: i64,
     pub camera_name: String,
+    pub ip_address: Option<String>,
     pub can_view: bool,
     pub can_control: bool,
 }
